2632c0e9cfd182e94997ec4298d3962233b87466
Eric Jizba <erijiz@microsoft.com>
2024-02-28 10:01:53 -0800

    localhost

--- src/scripts/streamRequest.ts
+++ src/scripts/streamRequest.ts
@@ -4,7 +4,7 @@ import { inputFilePath } from '../constants';
 
 async function streamRequest(): Promise<void> {
     const body = createReadStream(inputFilePath);
-    await fetch('http://localhost:7071/api/streamRequest', {
+    await fetch('http://127.0.0.1:7071/api/streamRequest', {
         method: 'POST',
         body,
         duplex: 'half',
--- src/scripts/streamResponse.ts
+++ src/scripts/streamResponse.ts
@@ -2,7 +2,7 @@ import { fetch } from 'undici';
 import { writeOutputFile } from '../writeOutputFile';
 
 async function streamResponse(): Promise<void> {
-    const response = await fetch(`http://localhost:7071/api/streamResponse`, {
+    const response = await fetch(`http://127.0.0.1:7071/api/streamResponse`, {
         method: 'GET',
     });
 
